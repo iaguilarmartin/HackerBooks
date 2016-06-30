c5869a768093e59c4559cda8aee7fc61fd9f816b
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-30 18:10:12 +0200

    Both controllers now listen for book selected change events trought delegate protocol and notifications

--- HackerBooks/PDFViewController.swift
+++ HackerBooks/PDFViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class PDFViewController: UIViewController {
     
-    let model: Book
+    var model: Book
     
     @IBOutlet weak var pdfWebView: UIWebView!
     
     init(model: Book) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
-        
-        self.title = self.model.title
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -28,11 +26,40 @@ class PDFViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.edgesForExtendedLayout = .None
+        
+        updateView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(newBookSelected), name: selectedBookChanged, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func updateView() {
+        self.title = self.model.title
+        
         if let maybePDF = try? DataDownloader.downloadExternalFileFromURL(self.model.document), pdf = maybePDF, baseURL = self.model.document.URLByDeletingLastPathComponent {
             
             pdfWebView.loadData(pdf, MIMEType: "application/pdf", textEncodingName: "", baseURL: baseURL)
         }
-
+    }
+    
+    func newBookSelected(notificarion: NSNotification) {
+        let info = notificarion.userInfo
+        
+        if let book = info?[selectedBookKey] as? Book {
+            self.model = book
+            updateView()
+        }
+        
     }
     
 }
