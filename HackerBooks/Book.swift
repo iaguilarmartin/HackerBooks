298251624eb6fa12d76b2271ed888455107376b3
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-07-07 19:07:16 +0200

    Book class is modified to add a delegate protocol. This protocol would be invoked when favorite property is modified. Also a notification is sent when that happen

--- HackerBooks/Book.swift
+++ HackerBooks/Book.swift
@@ -8,13 +8,26 @@
 
 import Foundation
 
-class Book {
+class Book: Equatable {
+    
+    static let bookChangedEvent = "BookChangedNotification"
+    static let bookChangedKey = "KeyBook"
     
     let title: String
     let authors: [String]
     let tags: [String]
     let image: NSURL
     let document: NSURL
+    var delegate: BookDelegate?
+    var isFavorite: Bool = false {
+        didSet {
+            if let delegate = delegate {
+                delegate.bookDelegate(self, favoriteValueChanged: self.isFavorite)
+            }
+            
+            NSNotificationCenter.defaultCenter().postNotificationName(Book.bookChangedEvent, object: self, userInfo: [Book.bookChangedKey: self])
+        }
+    }
     
     init (title: String, authors: [String], tags: [String], image: NSURL, document: NSURL) {
         self.title = title
@@ -23,4 +36,19 @@ class Book {
         self.image = image
         self.document = document
     }
+    
+    //MARK: - Proxies
+    var proxyForComparasion: String {
+        get {
+            return "\(title)\(authors.joinWithSeparator(","))\(tags.joinWithSeparator(","))"
+        }
+    }
+}
+
+func ==(lhs: Book, rhs: Book) -> Bool {
+    return lhs.proxyForComparasion == rhs.proxyForComparasion
+}
+
+protocol BookDelegate {
+    func bookDelegate(book: Book, favoriteValueChanged newValue:Bool)
 }
